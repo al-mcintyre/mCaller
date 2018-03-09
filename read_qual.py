df0e90589342dbe74b81696fd9d961c289d2515c
ralowe <r.lowe@qmul.ac.uk>
2018-03-09 16:52:02 +0000

    Added support for gzipped FASTQ files

--- read_qual.py
+++ read_qual.py
@@ -1,9 +1,15 @@
 from Bio import SeqIO
 import numpy as np
 import sys
+import gzip
 
 def extract_read_quality(fastqfi):
    read2qual = {}
-   for read in SeqIO.parse(fastqfi,"fastq"):
-      read2qual[read.id] = np.mean(read.letter_annotations["phred_quality"])
+   if fastqfi.find(".gz")!=-1:
+      with gzip.open(fastqfi, "rt") as handle:
+         for read in SeqIO.parse(handle, "fastq"):
+            read2qual[read.id] = np.mean(read.letter_annotations["phred_quality"])
+   else:
+      for read in SeqIO.parse(fastqfi,"fastq"):
+         read2qual[read.id] = np.mean(read.letter_annotations["phred_quality"])
    return read2qual
